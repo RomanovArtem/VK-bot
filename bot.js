52ec8df00d370253ab41c602a1b8063488924a19
RomanovArtem <temik_romanov@mail.ru>
2017-05-24 15:42:56 +0300

    Добавил вспомогательные функции для обращения к api

--- bot.js
+++ bot.js
@@ -2,11 +2,21 @@
  * Created by ArtemRomanov on 24.05.2017.
  */
 
-$.ajax({
-    url: 'https://api.vk.com/method/friends.search?count=60&fields=photo_100&access_token=bac05b6be300346ed88a2aaa5ac26939b327ec9c5f8d966507a8b62432251a8706ad4660237b911d170c8&expires_in=0&user_id=119397727&v=5.52',
-    method: 'GET',
-    dataType: 'JSONP',
-    success: function (data) {
-        console.log(data);
-    }
-});
+function getUrl(method, params) {
+    if (!method) throw new Error('Не указан метод');
+    params = params || {};
+    params['access_token'] = 'bac05b6be300346ed88a2aaa5ac26939b327ec9c5f8d966507a8b62432251a8706ad4660237b911d170c8';
+    return 'https://api.vk.com/method/' + method + '?' + $.param(params);
+}
+
+function sendRequest(method, params) {
+    $.ajax({
+        url: getUrl('friends.search', {count: 60, fields: 'photo_100'}),
+        method: 'GET',
+        dataType: 'JSONP',
+        success: function (data) {
+            console.log(data);
+        }
+    });
+}
+
